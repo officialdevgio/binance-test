bd3f4a4b49844ba7de0c080179d1f61c4b8b16c6
officialdevgio <officialdevgio@gmail.com>
2025-12-14 19:37:07 -0500

    Added port and cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,20 @@ import express from "express";
 import type { Request, Response } from "express";
 import ws from "ws";
 import io from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 import WebSocket, { WebSocket as WSClient } from "ws";
 
